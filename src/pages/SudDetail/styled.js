67ac332036fcbe5b0fb44bfc9a167e3cc01a8759
abdurahim_dev@raqamlilabarotiriya <ergashovabdurahim32@gmail.com>
2024-10-10 21:46:48 +0500

    media Finish Inshallah

--- src/pages/SudDetail/styled.js
+++ src/pages/SudDetail/styled.js
@@ -75,6 +75,9 @@ export const VideoSection = styled.div`
     height: 500px;
     border-radius: 10px;
     box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+    @media (max-width: 600px){
+      height: 300px;
+    }
   }
 `;
 
